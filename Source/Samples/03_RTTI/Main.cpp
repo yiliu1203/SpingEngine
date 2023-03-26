735ac4d6b6e22d6c491e3ea4bb9b77d2848a0b6f
yl <yiliu1203@163.com>
2023-03-26 10:10:37 +0800

    reflect success

--- Source/Samples/03_RTTI/Main.cpp
+++ Source/Samples/03_RTTI/Main.cpp
@@ -22,26 +22,21 @@ int main()
 
     std::cout << StaticTypeDecl<Foo>::name() << " " << StaticTypeDecl<Foo>::id() << std::endl;
 
-    Meta::Declare<Foo>().property("x", &Foo::x);
-
-    // Meta* m = new Meta(StaticTypeDecl<Foo>::id(), StaticTypeDecl<Foo>::name());
+    Meta::Declare<Foo>().property("x", &Foo::x).property("y", &Foo::y);
 
     const Meta* meta1 = reflect::MetaManager::Instance().Get("Foo");
     const Meta* meta2 = reflect::MetaManager::Instance().Get<Foo>();
     ASSERT(meta1 != nullptr);
     ASSERT(meta1 == meta2);
 
-    meta1->getProprity("x");
+    Foo F;
+    F.x = 1;
+    F.y = 2;
 
-
-
-    // Foo F;
-    // F.x = 1;
-    // F.y = 2;
-
-    // auto Prop = meta1->getProprity("x");
-    // Prop->get(&F);
-    // std::cout << "property Foo.x = " << std::endl;
+    auto propX = meta1->getProprity("x");
+    auto propY = meta1->getProprity("y");
+    std::cout << "property Foo.x = " << propX->get(&F).To<int>() << std::endl;
+    std::cout << "property Foo.y = " << propY->get(&F).To<int>() << std::endl;
 
 
     std::cout << std::endl << "Success";
